4a209c0ad5d3e93f1a230191d1b5e27122c76cee
krishnendhu12 <krishnendhumanoj77@gmail.com>
2025-06-24 12:59:38 +0530

    Create TablePrinter.py

---
+++ TablePrinter.py
@@ -0,0 +1,9 @@
+name=input("Krishnendhu: ")
+USN=input("1AY24BT023: ")
+tableData = [
+    ['apples', 'oranges', 'cherries', 'banana'],
+    ['Alice', 'Bob', 'Carol', 'David'],
+    ['dogs', 'cats', 'moose', 'goose']
+]
+for i in range(len(tableData[0])):
+    print(tableData[0][i], tableData[1][i], tableData[2][i])
