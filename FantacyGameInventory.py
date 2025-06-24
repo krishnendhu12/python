ee04c3f2436bebe4ef42c4ad3c3de36e1932bba6
krishnendhu12 <krishnendhumanoj77@gmail.com>
2025-06-24 12:57:30 +0530

    Create FantacyGameInventory.py

---
+++ FantacyGameInventory.py
@@ -0,0 +1,10 @@
+name=input("Krishnendhu: ")
+USN=input("1AY24BT023: ")
+inventory = {
+    'gold coin': 5,
+    'dagger': 1,
+    'rope': 2
+}
+print("Inventory:")
+for item in inventory:
+    print(item,inventory[item])
